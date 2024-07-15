3c54603360bcc5a4a058fe5cab9014f36d1e84df
update9520 <abhisheknegi9520514183@gmail.com>
2024-07-16 03:33:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://allfather9520.onrender.com"
+        URL = "https://allfather4183.onrender.com"
     else:
-        URL = "https://allfather9520.onrender.com"
+        URL = "https://allfather4183.onrender.com"
 
 
 
